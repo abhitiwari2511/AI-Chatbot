57ff1c53d07a019ad42ce9506cce8a194a83026c
abhitiwari2511 <abhishek.tiwari@hmritm.ac.in>
2025-04-26 18:44:42 +0530

    added footer

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -7,16 +7,16 @@ const Dashboard = () => {
   const [answer, setAnswer] = useState("");
 
   return (
-    <div className="bg-zinc-900 flex justify-center flex-col items-center h-screen w-screen">
+    <div className="bg-zinc-900 flex justify-center p-8 flex-col items-center h-screen w-screen">
       <h1 className="text-white text-2xl sm:text-4xl font-bold">AI Chat Bot</h1>
-      <div className="rounded-md scroll-auto bg-zinc-800 text-white p-8 h-[60%] w-[70%] sm:h-[70%] sm:w-[40%] m-6 sm:m-3">
+      <div className="rounded-md scroll-auto bg-zinc-800 text-white p-8 h-[60%] w-[80%] sm:h-[70%] sm:w-[40%] m-6 sm:m-3">
         {answer}
       </div>
       <div className="flex justify-between items-center sm:w-[50%] rounded-full">
         <input
           value={input}
           onChange={(e) => setInput(e.target.value)}
-          className="text-white text-md m-3 w-full bg-zinc-800 rounded-full p-3"
+          className="text-white text-sm sm:text-md m-3 w-full bg-zinc-800 rounded-full p-3"
           placeholder="Ask Anything..."
         />
         <button
@@ -25,6 +25,11 @@ const Dashboard = () => {
         >
           <FaArrowUp />
         </button>
+      </div>
+      <div>
+        <p className="text-white text-sm sm:text-md font-bold mt-2">
+          Made with ❤️ by{" Abhishek"}
+        </p>
       </div>
     </div>
   );
